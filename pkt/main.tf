c0ffc1d5544ebb425f14944d3b944e1c9ee3f687
Joao Luna <joaopedroluna98@gmail.com>
2022-07-20 23:23:25 +0100

    Add PKT

---
+++ pkt/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    akash = {
+      version = "0.3"
+      source  = "joaoluna.com/cloud/akash"
+    }
+  }
+}
+
+provider "akash" {}
+
+resource "akash_deployment" "pkt_deployment" {
+  count = var.number_instances
+
+  sdl = templatefile("${path.module}/sdl.yaml", {
+    wallet_addr = var.wallet_addr
+  })
+}
+
+output "deployment_id" {
+  value = akash_deployment.pkt_deployment
+}
